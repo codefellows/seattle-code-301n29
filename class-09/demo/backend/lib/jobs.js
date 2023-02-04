8ffe1b903c82210f4b2c130614f9b0a36f5c8f56
Roger Reyes <rogerrmr14@gmail.com>
2023-02-04 11:21:46 -0800

    Added demos and notes

---
+++ class-09/demo/backend/lib/jobs.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const axios = require('axios');
+
+function getJobs(req, res, next) {
+  const url = `https://boards-api.greenhouse.io/v1/boards/vaulttec/jobs?content=true`;
+
+  // Old way before Refactoring
+  // try {
+  //   let response = await axios.get(url);
+  //   let formattedJobData = response.data.jobs.map(jobObj => new Job(jobObj))
+  //   res.status(200).send(formattedJobData);
+  // }
+  // catch (err) {
+  //   next(err);
+  // }
+
+  // New Way Refactored
+  axios.get(url)
+    .then(data => {
+      let formattedJobData = data.data.jobs.map(jobObj => new Job(jobObj))
+      res.status(200).send(formattedJobData);
+    })
+    .catch(error => next(error));
+}
+
+class Job {
+  constructor(obj) {
+    this.name = obj.title;
+    this.description = obj.content;
+    this.departments = this.getNames(obj.departments);
+    this.offices = this.getNames(obj.offices);
+    this.location = obj.location.name;
+    this.url = obj.absolute_url;
+  }
+
+  getNames(arr) {
+    return arr.map(obj => obj.name);
+  }
+}
+
+module.exports = getJobs;
---
+++ class-09/demo/frontend/src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Header extends React.Component{
+  render(){
+    return(<h2>Jobs available at Vault Tec!</h2>)
+  }
+}
+
+export default Header
